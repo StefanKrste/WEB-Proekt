cc64f2733d80284ac435895726c369b358927d5d
StefanKrste <67862991+StefanKrste@users.noreply.github.com>
2021-11-14 00:57:51 +0100

    blog

--- funkcii.js
+++ funkcii.js
@@ -23,3 +23,40 @@ function Komentiraj(br){
     document.getElementById("vlezNaKom"+br).value="";
 }
 
+function dodadiblog(){
+    var naslov=document.getElementById("naslov").value;
+    var ime=document.getElementById("ime").value;
+    var prezime=document.getElementById("prezime").value;
+    var tekst=document.getElementById("txt").value;
+    var datum=document.getElementById("datum").value;
+    var tema=document.getElementById("tema").value;
+    if(naslov !== "" && ime !== "" && prezime !== "" && tekst !== "" && datum !== "" && tema !== ""){
+        var hr=document.createElement("hr");
+        var hed1=document.createElement("h1");
+        hed1.innerHTML=naslov;
+        hed1.style.marginLeft = "4%";
+        hed1.style.fontFamily = "'Ariel', cursive"
+        var hed2=document.createElement("h2");
+        hed2.innerHTML=tema;
+        hed2.style.color = "white";
+        hed2.style.fontFamily = "'Ariel', cursive"
+        hed2.style.marginLeft = "4%";
+        var par=document.createElement("P");
+        par.innerHTML=ime+" "+prezime+"&nbsp&nbsp&nbsp&nbsp"+datum;
+        var txt=document.createElement("P");
+        txt.innerHTML=tekst;
+        var div=document.createElement("div");
+        div.appendChild(hr);
+        div.appendChild(hed1);
+        div.appendChild(par);
+        div.appendChild(hed2);
+        div.appendChild(txt);
+        document.getElementById("blogovi").appendChild(div);
+        document.getElementById("naslov").value="";
+        document.getElementById("ime").value="";
+        document.getElementById("prezime").value="";
+        document.getElementById("txt").value="";
+        document.getElementById("datum").value="";
+        document.getElementById("tema").value="";
+    }
+}
